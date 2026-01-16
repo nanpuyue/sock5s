d42dc48aed6533204fc762a26b2b5509a8bd903b
nanpuyue <nanpuyue@gmail.com>
2026-01-16 14:05:54 +0800

    chore: upgrade dependencies

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6};
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
-use clap::{App, Arg};
+use clap::{Arg, Command};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{lookup_host, TcpListener, TcpStream, ToSocketAddrs, UdpSocket};
 use tokio_stream::{Stream, StreamExt};
@@ -33,22 +33,22 @@ mod util;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let matches = App::new("sock5s")
+    let matches = Command::new("sock5s")
         .version("0.2.2")
         .author("南浦月 <nanpuyue@gmail.com>")
         .about("A Simple Socks5 Proxy Server")
         .arg(
-            Arg::with_name("listen")
+            Arg::new("listen")
                 .short('l')
                 .long("listen")
                 .value_name("LISTEN ADDR")
                 .help("Specify the listen addr")
-                .takes_value(true)
+                .num_args(1)
                 .required(true),
         )
         .get_matches();
 
-    let listen = matches.value_of("listen").unwrap();
+    let listen = matches.get_one::<String>("listen").unwrap();
 
     let mut listener = Socks5Listener::listen(listen).await?;
 
